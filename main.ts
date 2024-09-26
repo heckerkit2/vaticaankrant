69eb4dac1dd0be429f10fedddca2b47fcf5a4ed2
heckerkit2 <robin_s@vbsdelinde.be>
2024-09-26 11:16:05 +0200

    New Update Promotion for my YT channel and Github

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 let Krant2 = "De mooiste kunstwerk vindt ik de sixtijnse kapel. Omdat het plafond heel mooi is."
 let Krant1 = "Vaticaanstad, het beste en kleinste land. Het heeft veel mooie musea en eigenlijk ook weinig. Waarom? Omdat het het kleinste land is!"
+let Krant3 = "YT = ZekerNietRobin Github = heckerkit2 2de Github = heckerkit"
 basic.forever(function () {
     input.onButtonPressed(Button.A, function(){
      basic.showString(Krant1)
@@ -7,4 +8,9 @@ basic.forever(function () {
 input.onButtonPressed(Button.B, function(){    
         basic.showString(Krant2)
     })
+    input.onButtonPressed(Button.AB, function() {
+    basic.showString(Krant3)
+    })
 })
+
+
